40f768281cc79ece86c2d1d99b33da3d26b1facc
Federico Ceratto <federico.ceratto@gmail.com>
2017-05-14 18:24:50 +0100

    Refactor

--- src/journal.py
+++ src/journal.py
@@ -5,11 +5,14 @@
 from systemd import journal
 
 
-def create_reader(level=journal.LOG_DEBUG, unit=None):
-    j = journal.Reader()
-    j.log_level(journal.LOG_INFO)
-    if unit is not None:
-        j.add_match(_SYSTEMD_UNIT=unit)
-    return j
+class JournalCommander():
+    def __init__(self):
+        self._cursors = {}  # unit -> position
+        self._selected_unit_names = set()
 
-
+    def create_reader(self, level=journal.LOG_DEBUG):
+        j = journal.Reader()
+        j.log_level(journal.LOG_INFO)
+        for unit_name in self._selected_unit_names:
+            j.add_match(_SYSTEMD_UNIT=unit_name)
+        return j
