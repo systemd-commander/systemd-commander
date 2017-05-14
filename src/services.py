0266b94275aeec61af7a6282d80d439b82b948e8
Federico Ceratto <federico.ceratto@gmail.com>
2017-05-14 17:49:14 +0100

    Add enable/disable

--- src/services.py
+++ src/services.py
@@ -16,6 +16,12 @@ def stop(name):
 def restart(name):
     subprocess.check_call([SYSTEMCTL_BINPATH, 'restart', name])
 
+def enable(name):
+    subprocess.check_call([SYSTEMCTL_BINPATH, 'enable', name])
+
+def disable(name):
+    subprocess.check_call([SYSTEMCTL_BINPATH, 'disable', name])
+
 def ls():
     elements = []
     out = subprocess.check_output([SYSTEMCTL_BINPATH, '-a', '--no-pager'])
